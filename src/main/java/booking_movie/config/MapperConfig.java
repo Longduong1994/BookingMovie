de8b89443281314aba01bbf2aeef04d2b6665446
Longduong1994 <128006465+Longduong1994@users.noreply.github.com>
2024-01-12 10:04:01 +0700

    Update MapperConfig.java

--- src/main/java/booking_movie/config/MapperConfig.java
+++ src/main/java/booking_movie/config/MapperConfig.java
@@ -1,5 +1,6 @@
 package booking_movie.config;
-
+import booking_movie.mapper.LocationMapper;
+import booking_movie.mapper.UserMapper;
 import org.mapstruct.factory.Mappers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
